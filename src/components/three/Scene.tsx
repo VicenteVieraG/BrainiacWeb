2865c63220a8feafa2a44e615878c77dfb9aa739
Vicente Viera <plantedcoot706@gmail.com>
2023-10-02 14:32:43 -0600

    Project Configuration and File System Declared

---
+++ src/components/three/Scene.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const Scene: FC = (): JSX.Element => {
+    return(
+        <div>
+            Ola Crayola xp
+        </div>
+    );
+}
+
+export default Scene;
